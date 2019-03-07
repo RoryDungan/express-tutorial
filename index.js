4d19e56fe49f2e20abf5b34ee2a1c6670b19b2ec
Rory Dungan <rorydungan@gmail.com>
2019-03-07 11:12:34 +1000

    Stub routes for server and static serve frontend files

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/api/v1/artists', (req, res) => {
+    res.status(500).send('Not implemented!')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.get('/api/v1/artist/:id', (req, res) => {
+    res.status(500).send('Not implemented!')
+})
+
+app.use(express.static('frontend/dist'))
+
+app.listen(port, () => console.log(`Setting phasers to stun... (port ${port})`))
