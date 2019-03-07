694a9c6b9f06ccb996821a127c004de0d7c6c630
Rory Dungan <rorydungan@gmail.com>
2019-03-07 11:21:29 +1000

    Add MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+'use strict'
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/api/v1/artists', (req, res) => {
-    res.status(500).send('Not implemented!')
+const { MongoClient } = require('mongodb')
+const assert = require('assert')
+const path = require('path')
+const mongoUrl = 'mongodb://localhost:27017'
+const dbName = 'music-map'
+
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+
+    const db = client.db(dbName)
+
+    app.get('/api/v1/artists', (req, res) => {
+        res.status(500).send('Not implemented!')
+    })
+
+    app.get('/api/v1/artist/:id', (req, res) => {
+        res.status(500).send('Not implemented!')
+    })
+
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+    app.listen(port, () => console.log(`Setting phasers to stun... (port ${port})`))
 })
 
-app.get('/api/v1/artist/:id', (req, res) => {
-    res.status(500).send('Not implemented!')
-})
-
-app.use(express.static('frontend/dist'))
-
-app.listen(port, () => console.log(`Setting phasers to stun... (port ${port})`))
